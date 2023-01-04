d247ba1e88735eb97b48538c2dced42cbd500fc4
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2023-01-04 22:22:11 +0530

    [Sartaj] : Added Reporting die Roll Count Until  Winning Position

--- SnakeAndLadder/SnakeAndLadderProblem.cs
+++ SnakeAndLadder/SnakeAndLadderProblem.cs
@@ -42,7 +42,7 @@ namespace SnakeAndLadder
                         }
                         break;
                 }
-
+                Console.WriteLine(this.position);
             }
         }
     }
